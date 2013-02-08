056d415557d3601571536a238a1fada88241fd28
Ilya <iseletsky@gmail.com>
2013-02-08 11:28:56 -0800

    Added documentation to the pool class.

--- Util/serial/Pool.h
+++ Util/serial/Pool.h
@@ -4,15 +4,37 @@
 #include <vector>
 #include <unordered_set>
 
+/**
+Useful for objects that are quickly destroyed and created all the time, like particles.
+This pool resizes itself it more room is needed because it internally contains a vector.
+
+The pool works by array indeces.  When you reserve a new object, you get its array index in the pool.
+When you free an object, you free it using its array index, releasing it back into the pool to be returned later.
+
+The objects in the pool are all preallocated and constructed so when you get a new object id back, just reinitialize that
+object from scratch.  2 phase initialization is good for objects managed in this pool.
+
+The IterablePool class defined below additionally keeps track of which ids are live so you can easily iterate over all live
+objects in the pool, to maybe update all live particles or something.
+*/
 template <typename T>
 class Pool {
 public:
+    /**
+    Creates the pool with some reserved size of elements.
+
+    @param reserve How many elements to have in the pool.  The pool will resize itself if more are needed.
+    */
     Pool(size_t reserve = 10) {
         m_elements.resize(reserve);
 
         clear();
     }
     
+    /**
+    Returns an id of a free element in the pool.  Now you can use this id to release the object
+    from the pool or for accessing the object.
+    */
     inline size_t getFreeId() {
         if(m_freeHead == m_elements.size()) {   //if no free
             m_elements.resize(m_elements.size() + 1);
@@ -26,7 +48,7 @@ public:
     }
 
     /**
-    Makes the vector allocate enough space for some size.
+    Resizes the pool to hold more elements if needed.  Does nothing if trying to make the pool smaller.
     */
     inline void reserveSpace(size_t size) {
         if(m_elements.size() < size) {
@@ -51,11 +73,19 @@ public:
         }
     }
         
+    /**
+    Frees an id to be reused in the future.
+    At this point, the object that this id refers to can't be considered valid anymore.
+    In fact it'll be corrupted now.
+    */
     inline void remove(size_t elementId) {
         m_elements[elementId].m_nextFree = m_freeHead;
         m_freeHead = elementId;
     }
 
+    /**
+    Kills all live objects, making the pool empty.  All ids are now invalid and released.
+    */
     inline void clear() { 
         m_freeHead = 0;
 
@@ -66,11 +96,17 @@ public:
         m_freeTail = m_elements.size() - 1;
     }
     
+    /**
+    Gets a constant reference to an element in the pool for an id.
+    */
     inline const T& get(size_t id) const {
         //TODO: make sure id is valid, at least in debug build
         return m_elements[id].m_element;
     }
 
+    /**
+    Gets a reference to an element in the pool for an id.
+    */
     inline T& get(size_t id) {
         //TODO: make sure id is valid, at least in debug build
         return m_elements[id].m_element;
@@ -92,6 +128,11 @@ private:
     size_t m_freeTail;                  ///<Set to size of the list if no tail
 };
 
+
+/**
+This is a wrapper around the pool class by maintaining a set of which ids are live.
+You can iterate over that set to get back all live objects.
+*/
 template <typename T>
 class IterablePool {
 public:
@@ -109,9 +150,6 @@ public:
         return res;
     }
 
-    /**
-    Makes the vector allocate enough space for some size.
-    */
     inline void reserveSpace(size_t size) {
         m_pool.reserveSpace(size);
     }
@@ -142,7 +180,8 @@ public:
     }
 
     /**
-    Returns the live set so you can iterate over it.
+    Returns the live set so you can iterate over it.  This is the set of ids,
+    so to access the object itself call get() for an id first.
     */
     const LiveSet& getLiveSet() const {
         return m_elementIds;
