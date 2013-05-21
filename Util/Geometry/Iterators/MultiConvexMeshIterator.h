63cc25d2e4c6b41653f74c44747750e6341cc65c
Ilya <iseletsky@gmail.com>
2013-05-20 23:16:50 -0700

    Changed the MultiConvexMeshIterator back to not alternating the sub iterator but doing them all in order.  This gave me the wrong results ll.

--- Util/Geometry/Iterators/MultiConvexMeshIterator.h
+++ Util/Geometry/Iterators/MultiConvexMeshIterator.h
@@ -33,27 +33,15 @@ public:
             LOG_FATAL_ERROR("calling getCurrentPosition() on multimesh iterator when at end");
         }
 
-        return m_iterators[m_currentIter % m_iterators.size()].getCurrentPosition();
+        return m_iterators[m_currentIter].getCurrentPosition();
     }
 
     /**
     Whether or not all cells in the iterator have been rasterized.
     */
     inline bool atEnd() const {
-        /*return m_currentIter >= m_iterators.size() 
-            || (m_currentIter == m_iterators.size() - 1 && m_iterators[m_currentIter].atEnd());*/
-
-        if(m_iterators.empty()) {
-            return true;
-        }
-
-        for(size_t iter = 0; iter < m_iterators.size(); iter++) {
-            if(!m_iterators[iter].atEnd()) {
-                return false;
-            }
-        }
-
-        return true;
+        return m_currentIter >= m_iterators.size() 
+            || (m_currentIter == m_iterators.size() - 1 && m_iterators[m_currentIter].atEnd());
     }
 
     /**
@@ -64,40 +52,14 @@ public:
             LOG_FATAL_ERROR("calling forward() on multimesh iterator when at end");
         }
 
-        do {
-            ++m_currentIter;
-        } while(m_iterators[m_currentIter % m_iterators.size()].atEnd());
+        if(m_iterators[m_currentIter].atEnd()) {
+            m_currentIter++;
+        }
+        else {
+            m_iterators[m_currentIter].forward();
 
-        //don't advance the iterator yet if it's its first time being switched to
-        if(m_currentIter >= m_iterators.size()) {
-            int numSkipped = 0;
-
-            //are you ready for some spagghetti code?  TODO: figure out how to unspaghettify it
-            while(true) {
-                if(m_iterators[m_currentIter % m_iterators.size()].atEnd()) {
-                    ++m_currentIter;
-                    ++numSkipped;
-
-                    if(numSkipped == m_iterators.size()) {
-                        return;
-                    }
-
-                    continue;
-                }
-
-                m_iterators[m_currentIter % m_iterators.size()].forward();
-                
-                if(m_iterators[m_currentIter % m_iterators.size()].atEnd()) {
-                    ++m_currentIter;
-                    ++numSkipped;
-
-                    if(numSkipped == m_iterators.size()) {
-                        return;
-                    }
-                }
-                else {
-                    return;
-                }
+            if(m_iterators[m_currentIter].atEnd()) {
+                m_currentIter++;
             }
         }
     }
